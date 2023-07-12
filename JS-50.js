9313ef0bafdce878d83d6bb29bd9f2867d714039
Revathi <Revathi092001@gmail.com>
2023-07-12 23:09:44 +0530

    javascript problems

--- JS-50.js
+++ JS-50.js
@@ -1,4 +1,4 @@
-// Write a JavaScript function to check whether an `input` is an array or not.
+// 1---Write a JavaScript function to check whether an `input` is an array or not.
 function check(arr){
     if(Array.isArray(arr)){
       return true;
@@ -12,13 +12,13 @@ function check(arr){
   console.log(check([ ]));
 
 
-// Write a JavaScript function to clone an array.
+// 2---Write a JavaScript function to clone an array.
 var arr_Clone = arra1 => arra1.slice(0);
 console.log(arr_Clone([5, 6, 9, 1]));
 console.log(arr_Clone([5, 2, [6, 1]]));
 
 
-// Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements of the array.
+// 3---Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements of the array.
 var firstele =  function(arr, n) {
     if (arr == null) 
     return void 0;
@@ -33,7 +33,7 @@ console.log(firstele([],3));
 console.log(firstele([7, 9, 0, -2],3));
 
 
-// Write a JavaScript function to get the last element of an array. Passing the parameter 'n' will return the last 'n' elements of the array.
+// 4--- Write a JavaScript function to get the last element of an array. Passing the parameter 'n' will return the last 'n' elements of the array.
 var lastele =  function(arr, n) {
     if (arr == null) 
       return void 0;
@@ -45,14 +45,14 @@ var lastele =  function(arr, n) {
   console.log(lastele([7, 9, 0, -2],3));
 
 
-// Write a JavaScript program to sort the items of an array.
+// 5--- Write a JavaScript program to sort the items of an array.
 let number = [0, 1, 2, 3, 10, 20, 30];
 number.sort((a, b) => a - b);
 
 console.log(number);
 
 
-//Write a simple JavaScript program to join all elements of the following array into a string.
+// 6--- Write a simple JavaScript program to join all elements of the following array into a string.
 var color=['Red', 'Green', 'Blue', 'Yellow'];
 console.log(color[1]);
 console.log(color.toString());
@@ -60,7 +60,7 @@ console.log(color.join());
 console.log(color.join('+'));
 
 
-//Write a JavaScript program that accepts a number as input and inserts dashes (-) between each even number. For example if you accept 025468 the output should be 0-254-6-8.
+// 7--- Write a JavaScript program that accepts a number as input and inserts dashes (-) between each even number. For example if you accept 025468 the output should be 0-254-6-8.
 const numb = 25468;
 const str= numb.toString();
 const result = [0,"-",str[0]];
@@ -75,7 +75,7 @@ for(let i = 1; i<str.length;i++){
 console.log(result.join(''));
 
 
-//Write a JavaScript program to find the most frequent item in an array.
+// 8--- Write a JavaScript program to find the most frequent item in an array.
 const findMostFrequentItem = arr => {
   const frequencyMap = new Map();
   arr.forEach(item => {
@@ -92,13 +92,12 @@ const findMostFrequentItem = arr => {
   });
   return mostFrequentItem;
 };
-// Test the function
 const array = [1, 'D', 'B', 2, 'B', 'A', 'A', 3, 'B', 1, 'C', 'C'];
 const mostFrequent = findMostFrequentItem(array);
 console.log(`The most frequent item is: ${mostFrequent}`);
 
 
-// Write a JavaScript program that accepts a string as input and swaps the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+// 9--- Write a JavaScript program that accepts a string as input and swaps the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
 function swap(str) {
   let newLetter = '';
   for (let i = 0; i < str.length; i++) {
@@ -114,7 +113,7 @@ function swap(str) {
   console.log(swap('The Quick Brown Fox'));
 
 
-// Write a JavaScript program that prints the elements of the following array.
+// 10--- Write a JavaScript program that prints the elements of the following array.
 const a = [
   [1, 2, 1, 24],
   [8, 11, 9, 4],
@@ -131,13 +130,13 @@ for (let i = 0; i < a.length; i++) {
 } 
 
 
-// Write a JavaScript program to find the sum of squares of a numerical vector.
+// 11--- Write a JavaScript program to find the sum of squares of a numerical vector.
 const vector = [1, 2, 3, 4, 5];
 const sumOfSquares = vector.reduce((sum, num) => sum + (num ** 2), 0);
 console.log("Sum of squares:", sumOfSquares);
 
 
-//Write a JavaScript program to compute the sum and product of an array of integers.
+// 12--- Write a JavaScript program to compute the sum and product of an array of integers.
 const array = [1, 2, 3, 4, 5];
 const sum = array.reduce((total, num) => total + num, 0); 
 const product = array.reduce((total, num) => total * num, 1);
@@ -145,7 +144,7 @@ console.log("Sum:", sum);
 console.log("Product:", product);
 
 
-// Write a JavaScript program to remove duplicate items from an array (ignore case sensitivity).
+// 14--- Write a JavaScript program to remove duplicate items from an array (ignore case sensitivity).
 const array1 = [1, 2, 3, 2, 4, 5, 1, 3, 4, 5, 6];
 const newArray = array1.filter((element, index, arr) => {
   return index === arr.indexOf(element);
@@ -153,7 +152,7 @@ const newArray = array1.filter((element, index, arr) => {
 console.log(newArray);
 
 
-/* We have the following arrays :color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+/* 15--- We have the following arrays :color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
 o = ["th","st","nd","rd"]
 Write a JavaScript program to display the colors in the following way :
 "1st choice is Blue ."
@@ -167,7 +166,7 @@ color.forEach((color, index) => {
 });
 
 
-// Write a JavaScript program to find the leap years in a given range of years.
+// 16--- Write a JavaScript program to find the leap years in a given range of years.
 const findLeapYears = (startYear, endYear) => {
   const leapYears = [];
   for (let year = startYear; year <= endYear; year++) {
@@ -183,14 +182,14 @@ const leapYears = findLeapYears(startYear, endYear);
 console.log("Leap Years:", leapYears);
 
 
-// Write a JavaScript program to shuffle an array.
+// 17--- Write a JavaScript program to shuffle an array.
 const shuffleArray = array => array.sort(() => Math.random()-0.5);
 const originalArray = [1, 2, 3, 4, 6];
 const shuffledArray = shuffleArray(originalArray);
 console.log("Shuffled Array:", shuffledArray);
 
 
-// Write a JavaScript program to perform a binary search.
+// 18--- Write a JavaScript program to perform a binary search.
 const binarySearch = (array, target) => {
   let low = 0;
   let high = array.length - 1;
@@ -198,11 +197,11 @@ const binarySearch = (array, target) => {
     const mid = Math.floor((low + high) / 2);
     const midValue = array[mid];
     if (midValue === target) {
-      return mid; // Found the target
+      return mid; 
     } else if (midValue < target) {
-      low = mid + 1; // Continue searching in the right half
+      low = mid + 1; 
     } else {
-      high = mid - 1; // Continue searching in the left half
+      high = mid - 1; 
     }
   }
   return -1; // Target not found
@@ -217,7 +216,7 @@ if (resultIndex !== -1) {
 }
 
 
-// There are two arrays with individual values. Write a JavaScript program to compute the sum of each individual index value in the given array.
+// 19--- There are two arrays with individual values. Write a JavaScript program to compute the sum of each individual index value in the given array.
 const computeSum = (array1, array2) => {
   const result = [];
   array1.forEach((value, index) => {
@@ -231,7 +230,7 @@ const sumArray = computeSum(array1, array2);
 console.log("Result Array:", sumArray);
 
 
-// Write a JavaScript program to find duplicate values in a JavaScript array.
+// 20--- Write a JavaScript program to find duplicate values in a JavaScript array.
 const findDuplicates = array => {
   const duplicateValues = [];
   array.forEach((value, index) => {
@@ -244,3 +243,145 @@ const findDuplicates = array => {
 const array = [1, 2, 3, 2, 4, 3, 5, 5, 1];
 const duplicates = findDuplicates(array); 
 console.log("Duplicate values:", duplicates);
+
+
+// 21--- Write a JavaScript program to flatten a nested (any depth) array. If you pass shallow, the array will only be flattened to a single level.
+const flattenArray = (array, shallow = false) =>
+array.reduce(
+  (result, element) =>      Array.isArray(element)
+      ? result.concat(shallow ? element : flattenArray(element))
+      : result.concat(element),
+  []
+);
+const nestedArray = [1, [2, [3, 4], 5], 6];
+const flattenedArray = flattenArray(nestedArray, true);
+console.log("Flattened Array:", flattenedArray);
+
+
+// 22--- Write a JavaScript program to compute the union of two arrays.
+const computeUnion = (arrayy1, arrayy2) => {
+  const combinedArray = [...arrayy1, ...arrayy2];
+  const uniqueElements = [...new Set(combinedArray)];
+  return uniqueElements;
+};
+const arrayy1 = [1, 2, 3];
+const arrayy2 = [100, 2, 1, 10];
+const union = computeUnion(arrayy1, arrayy2);
+console.log(union);
+
+
+// 23--- Write a JavaScript function to find the difference between two arrays.
+const findDifference = (arr1, arr2) => {
+  const difference = arr1.filter((element) => !arr2.includes(element));
+  return difference;
+};
+const arr1 = [1, 2, 3, 4, 5, 7];
+const arr2 = [3, 4, 5, 6];
+const difference = findDifference(arr1, arr2);
+console.log(difference);
+
+
+// 24--- Write a JavaScript function to remove. 'null', '0', '""', 'false', 'undefined' and 'NaN' values from an array.
+const removeFalseValues = (array) => {
+  const filteredArray = array.filter((element) => {
+    return !!element; 
+  });
+  return filteredArray;
+};
+const array = [0, null, "", false, undefined, NaN, 42, "hello"];
+const result = removeFalseValues(array);
+console.log(result);
+
+
+// 25--- Write a JavaScript function to sort the following array of objects by title value.
+const library = [
+  { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+  { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+  { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+];
+const sortLibraryByTitle = (arr) => {
+  const sortedArray = arr.sort((a, b) => {
+    const titleA = a.title.toLowerCase();
+    const titleB = b.title.toLowerCase();
+    if (titleA < titleB) return -1;
+    if (titleA > titleB) return 1;
+    return 0;
+  });
+  return sortedArray;
+};
+const sortedLibrary = sortLibraryByTitle(library);
+console.log(sortedLibrary);
+
+
+// 26--- Write a JavaScript program to find a pair of elements (indices of the two numbers) in a given array whose sum equals a specific target number.
+const findPairIndices = (numbers, target) => {
+  const indices = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        indices.push(i, j);
+        return indices;
+      }
+    }
+  }
+  return indices;
+};
+const numbers = [10, 20, 30, 40, 50, 60, 70];
+const target = 50;
+const pairIndices = findPairIndices(numbers, target);
+if (pairIndices.length) {
+  console.log(`Pair found at indices ${pairIndices[0]}, ${pairIndices[1]}`);
+} else {
+  console.log("No pair found");
+}
+
+
+// 27--- Write a JavaScript function to retrieve the value of a given property from all elements in an array.
+const getValues = (arr, value) => {
+  return arr.filter((element) => element && element[value]);
+};
+const sampleArray = [NaN, 0, 15, false, -22, '', undefined, 47, null];
+const result1 = getValues(sampleArray, 'valueOf');
+
+console.log(result1);
+
+
+// 28--- Write a JavaScript function to find the longest common starting substring in a set of strings.
+const longest_common_starting_substring = (strings) =>
+  strings.reduce((longestSubstring, currentString) => {
+    let commonSubstring = '';
+    for (let i = 0; i < longestSubstring.length; i++) {
+      if (longestSubstring[i] === currentString[i]) {
+        commonSubstring += longestSubstring[i];
+      } else {
+        break;
+      }
+    }
+    return commonSubstring;
+  });
+const strings = ['go', 'google'];
+const result2 = longest_common_starting_substring(strings);
+console.log(result2);
+
+
+// 29--- Write a JavaScript function to fill an array with values (numeric, string with one character) within supplied bounds.
+const num_range= (start, end, step) => {
+  const range = [];
+  for (let i = start.charCodeAt(0); i <= end.charCodeAt(0); i += step) {
+    range.push(String.fromCharCode(i));
+  }
+  return range;
+};
+const result4 = num_range('a', 'z', 2);
+console.log(result4);
+
+
+// 30--- Write a JavaScript function that merges two arrays and removes all duplicate elements.
+const merge_array = (array1, array2) => {
+  const mergedArray = [...array1, ...array2];
+  const uniqueArray = mergedArray.filter((value, index) => mergedArray.indexOf(value) === index);
+  return uniqueArray;
+};
+const array3 = [1, 2, 3];
+const array4 = [2, 30, 1];
+console.log(merge_array(array3, array4));
